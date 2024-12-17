384a0ce6e56808a841f1815793031152bce16adc
MarcoZinzeri <marksnine9007@gmail.com>
2024-12-17 11:14:02 +0000

    esercizio3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-lista=(("parmigiano","SC02",7),("sugo","SC03",2),(""))
+lista=(("parmigiano","SC02",7),("sugo","SC03",2),("mozzarella","SC03",3))
 @app.route("/")
 def homepage():
     return render_template("home.html",titolo="home")
@@ -10,5 +10,15 @@ def homepage():
 @app.route("/detail")
 def detail():
     return render_template("details.html",titolo="details",lista=lista)
+
+@app.route("/dettaglios/<nscaffale>")
+def scaffale(nscaffale):
+    listavuota=[]
+    for i in lista:
+        if i[1]==nscaffale:
+            listavuota.append(i)
+  
+    return render_template("dettaglioscaffale.html",titolo="scaffale",listavuota=listavuota, nscaffale=nscaffale)
+
 
 app.run(debug=True)
